835b89afae0876abc3587518dcb95f28bd2ccd91
deepashre <86648444+deepashre@users.noreply.github.com>
2022-03-09 14:23:34 +0530

    Update Provision-pcluster.sh

--- Provision-pcluster.sh
+++ Provision-pcluster.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-
-TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-export AWS_DEFAULT_REGION=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | cut -d \" -f4)
-yq -i ".Region=\"${REGION}\"" test.yaml
 echo "creating cluster-config.yaml file"
-source home/ubuntu/apc-ve/bin/activate
+source /home/ubuntu/apc-ve/bin/activate
 echo "Activated Virtual Environment"
-echo "Retrieving Tags from Running Instance"
-INSTANCE_ID=`wget -qO- http://instance-data/latest/meta-data/instance-id`
-REGION=`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed 's/.$//'`
-aws ec2 describe-tags --region $REGION --filter "Name=resource-id,Values=$INSTANCE_ID" --query 'Tags[*].{Key:Key,Value:Value}' | jq -r '.[] | select( .Key as $a | ["cost_resource", "project_name","researcher_name"] | index($a) )' >> out.json
-jq -s '.' out.json >> valid.json
-echo "valid.json file is updated with Tags"
-yq eval -P valid.json > valid.yaml
-echo "Json file is converted to yaml"
-sed -i '1 i\Tags:' valid.yaml
-yq eval-all "select(fileIndex == 1) *+ select(fileIndex == 0)" valid.yaml test.yaml >> test1.yaml
-echo "valid.yaml file and cluster-config.yaml file is merged into cluster-config1.yaml"
-echo "Modified cluster-config1.yaml with Tags"
-source home/ubuntu/apc-ve/bin/activate
-echo "Virtual Environmement Activated"
-echo "creating Cluster with updated cluster-config1.yaml"
-pcluster create-cluster --cluster-name test-cluster${RANDOM:0:1} --cluster-configuration test1.yaml
-echo "Success"
+scheduler=$1
+Region=$2
+if [ "$scheduler" == "slurm" ]; then
+       echo "slurm.yaml exists"
+       yq -i ".Region=\"$Region\"" slurm.yaml
+       echo "updated Region in slurm.yaml"
+else
+       echo "batch.yaml exists"
+       yq -i ".Region=\"$Region\"" batch.yaml
+       echo "updated Region in batch.yaml"
+fi
