d04b5990238513c6b4506774dbf90523d29e9365
deepashre <86648444+deepashre@users.noreply.github.com>
2022-03-10 10:43:56 +0530

    Update Provision-pcluster.sh

--- Provision-pcluster.sh
+++ Provision-pcluster.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo "creating cluster-config.yaml file"
 source /home/ubuntu/apc-ve/bin/activate
 echo "Activated Virtual Environment"
+echo "Retrieving Tags from Running Instance"
+INSTANCE_ID=`wget -qO- http://instance-data/latest/meta-data/instance-id`
+REGION=`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed 's/.$//'`
+aws ec2 describe-tags --region $REGION --filter "Name=resource-id,Values=$INSTANCE_ID" --query 'Tags[*].{Key:Key,Value:Value}' | jq -r '.[] | select( .Key as $a | ["cost_resource", "project_name","researcher_name"] | index($a) )' >> out.json
+jq -s '.' out.json >> valid.json
+echo "valid.json file is updated with Tags"
+yq eval -P valid.json > valid.yaml
+echo "Json file is converted to yaml"
+sed -i '1 i\Tags:' valid.yaml
 scheduler=$1
 Region=$2
 headnodeinstancetype=$3
@@ -23,7 +31,10 @@ if [ "$scheduler" == "slurm" ]; then
        yq -i ".Scheduling.SlurmQueues[0].ComputeResources[0].MaxCount=\"$maxvpc\"" slurm.yaml
        yq -i ".Scheduling.SlurmQueues[0].Networking.SubnetIds[0]=\"$computenodesubnetId\"" slurm.yaml
        sed 's/\"//g' slurm.yaml
-       echo "updated slurm.yaml"
+       yq eval-all "select(fileIndex == 1) *+ select(fileIndex == 0)" valid.yaml slurm.yaml >> cluster-config-slurm.yaml
+       echo "valid.yaml file and cluster-config.yaml file is merged into cluster-config-slurm.yaml"
+       echo "Modified cluster-config-slurm.yaml with Tags"
+       pcluster create-cluster --cluster-name test-cluster-$scheduler --cluster-configuration cluster-config-slurm.yaml
 else
        echo "batch.yaml exists"
        yq -i ".Region=\"$Region\"" batch.yaml
@@ -36,5 +47,9 @@ else
        yq -i ".Scheduling.AwsBatchQueues[0].ComputeResources[0].DesiredvCpus=\"$desiredvpc\"" batch.yaml
        yq -i ".Scheduling.AwsBatchQueues[0].Networking.SubnetIds[0]=\"$computenodesubnetId\"" batch.yaml
        sed 's/\"//g' batch.yaml
-       echo "updated batch.yaml"
+       yq eval-all "select(fileIndex == 1) *+ select(fileIndex == 0)" valid.yaml batch.yaml >> cluster-config-batch.yaml
+       echo "valid.yaml file and cluster-config.yaml file is merged into cluster-config-batch.yaml"
+       echo "Modified cluster-config-batch.yaml with Tags"
+       pcluster create-cluster --cluster-name test-cluster-$scheduler --cluster-configuration cluster-config-batch.yaml
+       
 fi
