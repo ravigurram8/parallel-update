73285ea9ad2be771d58470293c56de7f6e149d0c
pawanrlelab <104573988+pawanrlelab@users.noreply.github.com>
2022-06-10 16:13:34 +0530

    Update wait-stack-create.sh

--- wait-stack-create.sh
+++ wait-stack-create.sh
@@ -22,7 +22,7 @@ wait_stack_create() {
        INSTANCE_ID=`pcluster describe-cluster -n $1 --query headNode.instanceId`
       # PRIVATE_IP_ADDRESS=`pcluster describe-cluster -n $1 --query headNode.privateIpAddress`
        PARAMETER_NAME="/RL/RG/StandardCatalog/ParallelCluster-test/${STACK_NAME}"
-        aws ssm put-parameter --name "${PARAMETER_NAME}" --type "String" --value "${instance_id}"
+        aws ssm put-parameter --name "${PARAMETER_NAME}" --type "String" --value "${INSTANCE_ID}"
         echo "Instance id of the head node is stored on ${PARAMETER_NAME}"
         echo "Instance id is : ${INSTANCE_ID}"
 
