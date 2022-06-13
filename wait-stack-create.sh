eeb1ddb9d8596e58a65b6d9a231497e6b8144f7e
pawanrlelab <104573988+pawanrlelab@users.noreply.github.com>
2022-06-13 13:40:40 +0530

    Update wait-stack-create.sh

--- wait-stack-create.sh
+++ wait-stack-create.sh
@@ -6,27 +6,22 @@ wait_stack_create() {
     
 
     echo "Waiting for [$STACK_NAME] stack creation."
-
-    aws cloudformation wait stack-create-complete \
-    --region ${REGION}  \
-    --stack-name ${STACK_NAME}
+    aws cloudformation wait stack-create-complete --region ${REGION} --stack-name ${STACK_NAME}
     status=$?
 
     if [[ ${status} -ne 0 ]] ; then
         # Waiter encountered a failure state.
         echo "Stack [${STACK_NAME}] creation failed. AWS error code is ${status}."
-
-        exit ${status}
-        else
-        
+        exit 1
+    else        
        INSTANCE_ID=`pcluster describe-cluster -n $1 --query headNode.instanceId`
-      # PRIVATE_IP_ADDRESS=`pcluster describe-cluster -n $1 --query headNode.privateIpAddress`
+       # PRIVATE_IP_ADDRESS=`pcluster describe-cluster -n $1 --query headNode.privateIpAddress`
        PARAMETER_NAME="/RL/RG/StandardCatalog/ParallelCluster-test/${STACK_NAME}"
-        aws ssm put-parameter --name "${PARAMETER_NAME}" --type "String" --value "${INSTANCE_ID}"
-        echo "Instance id of the head node is stored on ${PARAMETER_NAME}"
-        echo "Instance id is : ${INSTANCE_ID}"
-
+       aws ssm put-parameter --name "${PARAMETER_NAME}" --type "String" --value "${INSTANCE_ID}"
+       echo "Instance id of the head node is stored on ${PARAMETER_NAME}"
+       echo "Instance id is : ${INSTANCE_ID}"
+       
     fi
-    return
+    exit 0
 }
 
